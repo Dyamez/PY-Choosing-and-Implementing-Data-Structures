4576754406d386500450f29b6d8e1fc8890baaf6
Dyamez <dyamez@outlook.com>
2023-10-15 12:26:32 -0700

    act

---
+++ arrays/1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+my_array = np.array([1,2,3,4])
+
+def print_array_values(arr):
+    for element in arr:
+        print(element)
+
+print_array_values(my_array)
---
+++ arrays/2.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+new_array = np.array([2,4,6,8])
+
+def array_num(arr):
+    arr_sum = 0
+    for num in arr:
+        arr_sum = arr_sum + num
