9bbde1a107317167434794e80837862ee0c07224
Dyamez <dyamez@outlook.com>
2023-10-15 14:18:55 -0700

    work

--- arrays/2.py
+++ arrays/2.py
@@ -6,3 +6,7 @@ def array_num(arr):
     arr_sum = 0
     for num in arr:
         arr_sum = arr_sum + num
+    return arr_sum
+
+print(array_num(new_array))
+
--- arrays/3.py
+++ arrays/3.py
@@ -1,1 +1,8 @@
+import numpy as np
 
+my_array = np.array([1,2,3,4,5,6])
+
+def mult_by_three(arr):
+    return 3 * arr
+
+print(mult_by_three(my_array))
